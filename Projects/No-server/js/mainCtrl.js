832c3bd8ec85ba093945ebb6e7ef54a23e9d9aad
dbarrett24 <dbarrett24@gmail.com>
2017-02-14 17:57:23 -0700

    Initial Commit _ NO Server
    Initial Commit _ NO Server

---
+++ Projects/No-server/js/mainCtrl.js
@@ -0,0 +1,8 @@
+angular.module("brawlApp").controller("mainCtrl", function($scope, mainService){
+// ***********************************   
+//                TEST 
+    // $scope.test = "Controller is working";
+    // $scope.test1 = mainService.test1;
+// ***********************************   
+
+});
---
+++ Projects/No-server/js/mainService.js
@@ -0,0 +1,4 @@
+angular.module("brawlApp").service("mainService", function(){
+    this.test1 = "Service is working";
+
+});
