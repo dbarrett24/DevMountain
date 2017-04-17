87e07a751c16f6ef90d5ea6e39f99aa705bd0bc0
dbarrett24 <dbarrett24@gmail.com>
2017-04-17 08:47:29 -0600

    course progress

--- Practice/Node and Express Course/library/app.js
+++ Practice/Node and Express Course/library/app.js
@@ -1,32 +1,54 @@
-const express = require('express');
+var express = require('express');
 
-const app = express();
-const port = 3000;
+var app = express();
 
-app.use(express.static( 'public'));
-app.use(express.static( 'src/views'));
+var port = process.env.PORT || 5000;
+var bookRouter = express.Router(); //allows us to start building routes
 
-app.get('/', function(req, res, next){
-    res.send('Hello World');
+app.use(express.static('public'));
+app.set('views', './src/views');
+
+app.set('view engine', 'ejs');
+
+bookRouter.route('/')
+    .get(function(req, res, next){
+        res.render('Books'),{
+        title: 'Books',
+        nav: [{
+            Link:'/Books', 
+            Text: 'Books'
+        }, {
+            Link: '/Authors',
+            Text: 'Authors'
+        }]
+    }
+    });
+
+bookRouter.route('/single')
+    .get(function(req, res, next){
+        res.send('Hello Single Book');
+    });
+
+app.use('/Books', bookRouter) //Middleware that applies to all /Books routes, using bookRouter
+
+
+
+
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [{Link:'/Books', Text: 'Books'
+    }, {
+        Link: './Authors',
+        Text: 'Authors'
+    }]
+    });
 });
 
-
-app.get('/books', function(req, res, next){
+app.get('/books', function (req, res) {
     res.send('Hello Books');
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function(err){
-    console.log('Listening on port', port)
+app.listen(port, function (err) {
+    console.log('running server on port ' + port);
 });
