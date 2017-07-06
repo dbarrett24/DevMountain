9e45f7705da2e727d3cedffc8d856af883b76a19
dbarrett24 <dbarrett24@gmail.com>
2017-07-06 17:19:39 -0600

    nodeassessment practice

--- Certification-Assessments/Node/NodeAttempt2/node-assessment-postman/server.js
+++ Certification-Assessments/Node/NodeAttempt2/node-assessment-postman/server.js
@@ -10,7 +10,14 @@ app.use(bodyParser.json());
 
 //Endpoints
 app.get('/api/users', usersCtrl.getAllUsers);
-// app.get('/api/users?age=40', usersCtrl.getUsersByAgeLimit);
+app.get('/api/users/:id', usersCtrl.getUserById);
+app.get('/api/admins/', usersCtrl.getAdmins);
+app.get('/api/nonadmins/', usersCtrl.getNonAdmins);
+app.get('/api/user_type/:userType', usersCtrl.getUsersByType);
+app.put('/api/users/:userId', usersCtrl.updateUserById);
+app.post('/api/users/', usersCtrl.addUser);
+app.delete('/api/users/:userId', usersCtrl.deleteUser);
+
 
 
 
--- Certification-Assessments/Node/NodeAttempt2/node-assessment-postman/usersCtrl.js
+++ Certification-Assessments/Node/NodeAttempt2/node-assessment-postman/usersCtrl.js
@@ -1,5 +1,4 @@
 const userData = require('./userData.json');
-const userFilters = require('./userFilters');
 module.exports = {
     getAllUsers: function(req, res, next){
         if(req.query != {}){
@@ -49,8 +48,94 @@ module.exports = {
             return res.status(200).json(result);
 
         }
+    },
+    getUserById: function(req, res, next){
+        var id = req.params.id;
+        console.log(id);
+
+        for(var i = 0; i < userData.length; i++){
+            if(userData[i].id == id){
+                return res.status(200).json(userData[i])
+            }
+            else{
+                return res.status(404).send('null');
+            }
+        }
+    },
+    getAdmins: function(req, res, next){
+        var admins = [];
+        for(var i = 0; i < userData.length; i++){
+            if(userData[i].type === "admin"){
+                admins.push(userData[i]);
+            }
+        }
+        return res.status(200).send(admins)
+    },
+    getNonAdmins: function(req, res, next){
+        var nonAdmins = [];
+        for (var i = 0; i < userData.length; i++) {
+            if (userData[i].type !== "admin") {
+                nonAdmins.push(userData[i]);
+            }
+        }
+        return res.status(200).send(nonAdmins)
+    },
+    getUsersByType: function(req, res, next){
+        var userType = req.params.userType;
+        console.log(userType)
+        var users = [];
+        
+        for(var i = 0; i < userData.length; i++){
+            if(userData[i].type == userType){
+                users.push(userData[i]);
+            }
+        }
+        return res.status(200).send(users);
+    },
+    updateUserById: function(req, res, next){
+        var userId = req.params.userId;
+        console.log(req.params.userId);
+        
+        for(var i = 0; i < userData.length; i++){
+            if(userData[i].id == userId){
+                Object.assign(userData[i], req.body);
+            }
+        }
+        return res.status(200).json(userData);
+    },
+    addUser: function(req, res, next){
+        var newUser = req.body;
+        newUser.id = userData.length + 1;
+        console.log(newUser);
+        userData.push(newUser);
+
+        return res.status(200).send(userData);
+    },
+    deleteUser: function(req, res, next){
+        userId = req.params.userId;
+        console.log(userId);
+
+        for(var i = 0; i < userData.length; i++){
+            if(userData[i].id == userId){
+                userData.splice(i, 1);
+                return res.status(200).send(userData);
+            }
+            else{
+                next;
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
     // getUsersByAgeLimit: function(){
     //     var filteredByAgeLimit = userData.filter(function(user){
     //         return user.age < req.query.age;
